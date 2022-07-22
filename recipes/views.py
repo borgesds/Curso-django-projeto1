55179401c6fe3fa0d6d320b44e17cd8322db5df0
Borges <borges.an@hotmail.com>
2022-07-21 21:12:19 -0300

    up templates backend

--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,9 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+
 
 def home(request):
-    return HttpResponse('HOME 1')
+    return render(request, 'recipes/home.html')
 
 def contato(request):
     return HttpResponse('CONTATO 1')
