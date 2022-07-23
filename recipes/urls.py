8abfa0d37dc0541d53e140627dd402865ef24b73
André Borges <borges.an@hotmail.com>
2022-07-23 13:43:08 -0300

    up Removed contact and about

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,13 +4,9 @@ from django.urls import path
 Esta buscando o def home, contato,
 sobre dentro da pasta recipes que tem o arquivo home
 """
-from recipes.views import contato, home, sobre
+from recipes.views import home
 
 urlpatterns = [
     # Home
     path('', home),
-    # Sobre
-    path('sobre/', sobre),
-    # Contato
-    path('contato/', contato),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,6 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
     return render(request, 'recipes/home.html')
     # automaticamente busca pasta templates/...
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
-    # automaticamente busca pasta templates/...
-
-
-def sobre(request):
-    return HttpResponse('SOBRE 1')
